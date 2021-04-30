582c75f7c119d08a761260061bbca5dccf94f651
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2021-04-30 11:04:41 +0600

    Lab Task Complete

---
+++ Lab_1_180042135/Lab Task/app.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const contents = require("./loadContent");
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.write(contents.indexPage);
+        res.end();
+    } 
+    else if (req.url == "/about") {
+        res.write(contents.aboutPage);
+        res.end();
+    }
+    else if (req.url == "/blog") {
+        res.write(contents.blogPage);
+        res.end();    
+    } 
+    else if (req.url == "/contact") {
+        res.write(contents.contactPage);
+        res.end();    
+    }
+    else if (req.url == "/index") {
+        res.write(contents.indexPage);
+        res.end();    
+    }
+    else if (req.url == "/pricing") {
+        res.write(contents.pricingPage);
+        res.end();    
+    }
+    else if (req.url == "/services") {
+        res.write(contents.servicesPage);
+        res.end();    
+    }
+    else if (req.url == "/work") {
+        res.write(contents.workPage);
+        res.end();    
+    }else {
+        res.write("<h1>Page deosn't exist.</h1><br><a href = '/'>Go back to Base</a>");
+        res.end();
+    } 
+});
+
+server.listen(2135);
