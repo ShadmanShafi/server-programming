bdeca91f2bb971386b23bd4ba1bb775d724b7f56
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2021-04-30 10:13:44 +0600

    Completed HTTP Module

--- Lab_1_180042135/http-module.js
+++ Lab_1_180042135/http-module.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write("<h1>Konnichiwa</h1>");
-    res.end();
+    if (req.url == "/") {
+        res.write("<h1>This is the base URL.</h1>");
+        res.end();
+    } 
+    else if (req.url == "/home") {
+        res.write("<h1>This is Home Page.</h1>");
+        res.end();
+    } else {
+        res.write("<h1>Non-existent Page.</h1><br><a href = '/'>Go to Base</a>");
+        res.end();
+    } 
 });
 
 module.exports = { server };
