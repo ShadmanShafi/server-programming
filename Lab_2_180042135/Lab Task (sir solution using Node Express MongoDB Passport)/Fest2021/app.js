f27f46dc4fe164f062efa87361ff58acbf4ecdd5
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2021-07-02 11:53:26 +0600

    Created user routes but broke CSS of Login and Register Pages

--- Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/app.js
+++ Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/app.js
@@ -8,7 +8,9 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //Routes
-const indexRoutes = require('./routes/index.routes');
-app.use(indexRoutes)
+const indexRoutes = require("./routes/index.routes");
+const userRoutes = require("./routes/users.routes");
+app.use(indexRoutes);
+app.use("/users", userRoutes);
 
 module.exports = app;
---
+++ Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/controllers/users.controller.js
@@ -0,0 +1,22 @@
+const getLogin = (req, res)=>{
+    res.render("users/login.ejs");
+}
+
+const postLogin = ()=>{
+
+}
+
+const getRegister = (req, res)=>{
+    res.render("users/register.ejs");
+}
+
+const postRegister = ()=>{
+    
+}
+
+module.exports = {
+    getLogin,
+    getRegister,
+    postLogin,
+    postRegister,
+}
