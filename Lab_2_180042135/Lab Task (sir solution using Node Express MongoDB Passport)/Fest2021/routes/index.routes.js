56c7afff80a7fc3c5bc81b8472ad68dcc1530534
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2021-07-02 11:25:05 +0600

    Completed importing templates

--- Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/routes/index.routes.js
+++ Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/routes/index.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/",(req, res)=>{
     //res.send("<H1>Welcome!</H1>");
-    res.render("dashboard.ejs");
+    res.render("users/register.ejs");
 });
 
 module.exports = router; 
