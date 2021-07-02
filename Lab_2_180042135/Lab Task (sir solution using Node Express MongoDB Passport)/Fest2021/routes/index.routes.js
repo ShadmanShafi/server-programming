3966ffd887474611c6b6c265a790e2e72c7ef769
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2021-07-02 14:57:32 +0600

    Only registered users can view dashboard

--- Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/routes/index.routes.js
+++ Lab_2_180042135/Lab Task (sir solution using Node Express MongoDB Passport)/Fest2021/routes/index.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const ensureAuthenticated = require('./../middlewares/auth.middleware');
 
 router.get("/",(req, res)=>{
     //res.send("<H1>Welcome!</H1>");
     res.render("welcome.ejs");
 });
 
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
     res.render("dashboard.ejs");
   });
 
